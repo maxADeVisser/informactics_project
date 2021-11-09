efaf8fad514dcfe288eb17036e432d09cc177bd6
Max de Visser <m.madv96@gmail.com>
2021-11-09 10:50:54 +0100

    Update radarchart_plotly_express1.py
    tilføjede at man kan definerer hvor mangel kategorier man vil have vist svar for, så det ikke bare er alle sammen hver gang

--- radarchart_plotly_express1.py
+++ radarchart_plotly_express1.py
@@ -4,8 +4,13 @@ import plotly.graph_objects as pgo
 import pandas as pd
 # api token for REDcap: DD0E88BF0EF8E12ACD577F664A515337
 
+all_scales = ['goitre39', 'hyperthyroidscore', 'hypothyroidscore', 'eyescore', 'tirednessscore',
+                    'cognitivescore', 'anxietyscore', 'depressivityscore', 'emotionalscore', 'sociallifescore',
+                    'dailylifescore','cosmeticscore','overallqolscore','compositescore']
 
-def fetch_data_list(recordID): # contacts the API and returns a 2D list with thypro scores for a recordID
+# ------------------------------------------------------------------------------------ #
+
+def fetch_n_data_list(recordID, scales): # contacts the API and returns a 2D list with thypro scores for a recordID
     api_call = { 
         'token': 'DD0E88BF0EF8E12ACD577F664A515337', # api token
         'content': 'record',
@@ -14,16 +19,12 @@ def fetch_data_list(recordID): # contacts the API and returns a 2D list with thy
         'records[0]': f'{recordID}', # hvilken record du vil svar ud for
         'fields[0]': '', # er initially None
         'forms[0]': 'record_id', # specifies we are searching with a recordID
-        'returnFormat': 'json'
-    }
+        'returnFormat': 'json'}
+        
+    thypro_scale_values= {}
+    for scale in scales: # makes the list for holding the data
+        thypro_scale_values[f'{scale}'] = 0 # tilføjer skalaen til dicten
 
-    thypro_scale_values ={'goitre39' : 0, 'hyperthyroidscore' : 0, 'hypothyroidscore' : 0, 'eyescore' : 0, 'tirednessscore' : 0,
-                    'cognitivescore' : 0, 'anxietyscore' : 0, 'depressivityscore' : 0, 'emotionalscore' : 0, 'sociallifescore' : 0,
-                    'dailylifescore' : 0, 'cosmeticscore' : 0, 'overallqolscore' : 0, 'compositescore' : 0}
-
-    scales_raw = list(thypro_scale_values.keys()) # makes a list of the keys in the dict thypro_scale_values
-
-    for scale in scales_raw: # makes the list of holding the data
         api_call['fields[0]'] = scale
         response = requests.post('https://redcap.regionh.dk/api/', data=api_call) # contacts the API and gets the response
 
@@ -39,27 +40,50 @@ def fetch_data_list(recordID): # contacts the API and returns a 2D list with thy
             print(f"HTTP get request failed with {str(response.status_code)} status code")
 
     values_raw = [float(x) for x in list(thypro_scale_values.values())] # a list with only the values from thypro_scale_values dict
+    scales_raw = [x for x in list(thypro_scale_values.keys())]
     all_data = [scales_raw, values_raw]
+    print(all_data)
 
     return all_data
 
-# ------------------------------------------------------------------------------------ # 
+# ------------------------------------------------------------------------------------ #
 
-def plot_thyPRO_data(scales, values, values1=None): # function for plotting ThyPRO data
-    dataframe = pd.DataFrame(dict(r=values, theta=scales)) # creates a pandas dataframe with the inputs
+def plot_radar(scales, values, values1=None, values2=None): # function for plotting ThyPRO data
+    fig = pgo.Figure() 
 
-    fig = pgo.Figure()
+    fig.add_trace(pgo.Scatterpolar(r=values, theta=scales, name='Record A'))
 
-    fig.add_trace(pgo.Scatterpolar(r=values, theta=scales, fill='toself', name='Record A'))
-    fig.add_trace(pgo.Scatterpolar(r=values1, theta=scales, fill='toself', name='Record B'))
-    fig.update_layout(polar=dict(radialaxis=dict(visible=True,range=[0, 100])),showlegend=False)
+    if(values1 != None):
+        fig.add_trace(pgo.Scatterpolar(r=values1, theta=scales, name='Record B'))
+
+    if(values2 != None):
+        fig.add_trace(pgo.Scatterpolar(r=values2, theta=scales, name='Record C'))
+    
+    fig.update_layout(polar=dict(radialaxis=dict(visible=True,range=[0, 100])),showlegend=True)
     fig.show()
 
 # ------------------------------------------------------------------------------------ # 
 
-# --- Main --- #
-# records that work: 28, 27
-thypro_results1 = fetch_data_list(27) # method call - returns a 2D list
-thypro_results = fetch_data_list(28)
+def plot_line(scales_list, recordID_list): # er ind the making
 
-plot_thyPRO_data(thypro_results[0], thypro_results[1], thypro_results1[1]) # method call for plotting the data
+    all_values_for_single_scale = []
+
+    for scale in scales_list:
+        for recordID in recordID_list:
+            data_result = fetch_n_data_list(recordID, scale) # returnerer 2D liste med resultaterne
+            all_values_for_single_scale.append(data_result[1])
+
+    dataframe = pd.DataFrame(dict(r=values, theta=scale))
+    fig = px.line(dataframe, x=scale, y=all_values_for_single_scale, title='ThyPRO Score')
+    
+    fig.show()
+
+# ------------------------------------------------------------------------------------ # 
+
+
+# --- Main --- #
+# records that work: 29, 28, 27
+#thypro_results2 = fetch_full_data_list(24)
+#thypro_results1 = fetch_full_data_list(27) # method call - returns a 2D list
+thypro_results = fetch_n_data_list(28, ['goitre39', 'hyperthyroidscore', 'hypothyroidscore'])
+plot_radar(thypro_results[0], thypro_results[1]) # method call for plotting the data
